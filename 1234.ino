a1e080ddad1ef5652954becb067084ca593bab50
pattharajarin <120165932+pattharajarin@users.noreply.github.com>
2023-03-10 00:47:31 +0700

    Update 1234.ino

--- 1234.ino
+++ 1234.ino
@@ -1,165 +1,125 @@
 #include <Servo.h>
-#include <Bounce2.h>
+
+int ledPinRed = 5;
+int ledPinYellow = 6;
+int ledPinGreen = 7;
+
+int buzzerPin = 12;
 
 int trigPin = 10;
 int echoPin = 11;
-int ledPinR = 5;
-int ledPinY = 6;
-int ledPinG = 7;
-int buzzerPin = 12;
+
+int pirSensorPin = 13;
+
 int rainSensorPin = A0;
 
-// กำหนดพินสำหรับ LDR sensor และ servo motor
-const int ldrPin = A1;
-const int servoPin1 = 8;
-const int servoPin2 = 9;
+int ldrSensorPin = A1;
 
-// กำหนดค่าคงที่สำหรับอ่านค่า LDR
-const int min_light = 0;
-const int max_light = 1023;
+int buttonPin = 4;
 
-int minimumDistance = 10; // ระยะต่ำสุดที่ตรวจจับได้ (ซม.)
-int maximumDistance = 100; // ระยะสูงสุดที่ตรวจจับได้ (ซม.)
-int maximumRainValue = 300; // ค่าอ่านของ Rain Sensor ที่ถือว่าเป็นฝนตก
-
-// กำหนดชื่อพอร์ตที่เชื่อมต่อ push button
-const int buttonPin = 4;
-
-// กำหนดตัวแปรเก็บขา PIR Sensor
-int pirPin = 13;
-
-// สร้าง object ของ library Bounce
-Bounce button = Bounce();
-
-// สร้างอ็อบเจ็กต์สำหรับ Servo
-Servo myServo;
+//boolean prevBtnStatus = LOW;
 
 void setup() {
-  // เริ่มต้นค่า LDR sensor และ servo motor
-  pinMode(ldrPin, INPUT);
-  myServo.attach(servoPin1);
-  myServo.attach(servoPin2);
-  myServo.write(45);
+  pinMode(ledPinRed, OUTPUT);
+  pinMode(ledPinYellow, OUTPUT);
+  pinMode(ledPinGreen, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
 
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
-  pinMode(ledPinR, OUTPUT);
-  pinMode(ledPinY, OUTPUT);
-  pinMode(ledPinG, OUTPUT);
-  pinMode(buzzerPin, OUTPUT);
 
-  // กำหนด pinMode และเปิดการใช้งาน debounce สำหรับ push button
-  pinMode(buttonPin, INPUT_PULLUP);
-  button.attach(buttonPin);
-  button.interval(5);
+  pinMode(pirSensorPin, INPUT);
+  pinMode(ldrSensorPin, INPUT);
+  pinMode(buttonPin, INPUT);
 
-  // กำหนดให้ขา PIR Sensor เป็น Input
-  pinMode(pirPin, INPUT);
 
-  // กำหนดให้ขา Servo เป็น Output
-  myServo.attach(servoPin1);
-  myServo.attach(servoPin2);
-
-  // เปิด Serial communication สำหรับ debugging
   Serial.begin(9600);
 }
 
-void loop() {
-  // อ่านค่าจาก LDR sensor
-  int light_value = analogRead(ldrPin);
+void OpenLEDRed(bool Red){
+  digitalWrite(ledPinRed, Red);
+}
+void OpenLEDYellow(bool Yellow){
+  digitalWrite(ledPinYellow, Yellow);
+}
+void OpenLEDGreen(bool Green){
+  digitalWrite(ledPinGreen, Green);
+}
+void Buzzer(){
+}
 
-  // คำนวณค่ามุมของ servo motor จากค่าความสว่าง
-  int angle = 45 + int((light_value - min_light) * (90.0 / (max_light - min_light)));
-
-  // ควบคุม servo motor ตามค่ามุมที่คำนวณได้
-  myServo.write(angle);
-
-  // อ่านค่าจาก Ultrasonic Sensor
+long microsecondsToCentimeters(long microseconds){
+  return microseconds / 29 / 2;
+}
+void UltrasonicSensor() {
+  long duration, distance;
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-  long duration = pulseIn(echoPin, HIGH);
-  int distance = duration / 58;
+
+  duration = pulseIn(echoPin, HIGH);
+  distance = microsecondsToCentimeters(duration);
 
   Serial.print("Distance: ");
   Serial.print(distance);
   Serial.println(" cm");
+  delay(500);
+}
 
-  // ตรวจจับฝน
-  int rainValue = analogRead(rainSensorPin);
-
-  Serial.print("Rain Value: ");
-  Serial.println(rainValue);
-
-  // อ่านสถานะของ push button
-  button.update();
-
-  // อ่านค่าจาก PIR Sensor
-  int pirState = digitalRead(pirPin);
-
-  // ควบคุม LED ด้วยค่าความสว่าง
-  if (light_value < 500) {
-    digitalWrite(ledPinR, HIGH);
-    digitalWrite(ledPinY, HIGH);
-    digitalWrite(ledPinG, HIGH);
-  } else {
-    digitalWrite(ledPinR, LOW);
-    digitalWrite(ledPinY, LOW);
-    digitalWrite(ledPinG, LOW);
-  }
-  // ถ้ามีน้ำท่วมเกินค่าที่กำหนด
-  if (distance <= minimumDistance && rainValue >= maximumRainValue) {
-    digitalWrite(ledPinR, HIGH); // เปิด LED
-    digitalWrite(ledPinY, HIGH);
-    digitalWrite(ledPinG, HIGH);
-    tone(buzzerPin, 1000); // เล่นเสียง Buzzer
-    delay(500);
-    noTone(buzzerPin);
-    delay(500);
-    myServo.write(90); // หมุน Servo ไปที่มุม 90 องศา
+void CheckPIR(int pirSensorPin){
+  if (digitalRead(pirSensorPin) == HIGH) {
+    Serial.println("Motion detected!");
+    delay(100);
   }
   else {
-    digitalWrite(ledPinR, LOW); // ปิด LED
-    digitalWrite(ledPinY, LOW);
-    digitalWrite(ledPinG, LOW);
-    myServo.write(0); // หมุน Servo ไปที่มุม 0 องศา
+    Serial.println("No motion detected.");
+    delay(100);
   }
-  // ถ้ามีการกด push button
-  if (button.fell()) {
-    // หยุดการทำงานทั้งหมด
-    while (true) {
-      digitalWrite(ledPinR, HIGH);
-      digitalWrite(ledPinY, HIGH);
-      digitalWrite(ledPinG, HIGH);
-      delay(500);
-      digitalWrite(ledPinR, LOW);
-      digitalWrite(ledPinY, LOW);
-      digitalWrite(ledPinG, LOW);
-      delay(500);
-    }
-  }
-  // ถ้าตรวจพบการเคลื่อนไหว หยุดการทำงานของ Servo
-  if (pirState == HIGH) {
-    myServo.detach();
-  }
-  // ถ้าไม่ตรวจพบการเคลื่อนไหว กำหนดให้ Servo เคลื่อนไหวตามปกติ
-  else {
-    myServo.attach(servoPin1);
-    myServo.attach(servoPin2);
-    myServo.write(90);
-    delay(1000);
-    myServo.write(0);
-    delay(1000);
-  }
+}
 
-  // ส่งค่าความสว่างและมุมของ servo motor ออกไปทาง Serial communication
-  Serial.print("Light Value: ");
-  Serial.print(light_value);
-  Serial.print(", Angle: ");
-  Serial.println(angle);
-
-  // รอเพื่อให้ข้อมูลถูกอัพเดต
+void CheckRain(int rainSensorPin){
+  int valueRain = analogRead(rainSensorPin);
+  Serial.print("Rain Sensor value: ");
+  Serial.println(valueRain);
   delay(100);
+  //if(value <= 180){
+  //  digitalWrite(ledPinRed, HIGH);
+  //  digitalWrite(ledPinYellow, HIGH);
+  //  digitalWrite(ledPinGreen, HIGH);
+  //  digitalWrite(buzzerPin, HIGH);
+  //}
+  //else{
+  //  digitalWrite(ledPinRed, LOW);
+  //  digitalWrite(ledPinYellow, LOW);
+  //  digitalWrite(ledPinGreen, LOW);
+  //  digitalWrite(buzzerPin, LOW);
+  //}
+}
+
+void CheckLDR(int ldrSensorPin){
+  int valueLDR = analogRead(ldrSensorPin);
+  Serial.print("LDR Sensor Value = ");
+  Serial.println(valueLDR);
+  delay(500);
+}
+
+void EmergencyStop(){
+  OpenLEDRed(false);
+  OpenLEDYellow(false);
+  OpenLEDGreen(false);
+}
+
+void ButtomSwitch() {
+  boolean btnStatus = digitalRead(buttonPin);
+  if (btnStatus == HIGH && prevBtnStatus == LOW) {
+    EmergencyStop();
+    delay(200);
+  }
+  //prevBtnStatus = btnStatus;
+}
+
+void loop(){
+
 }
